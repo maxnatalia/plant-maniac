26943426c16c8aafb45c622d7f629e4b4efd2229
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-09 10:10:00 +0100

    added new type of common Paragraph styles

--- src/common/styles/Paragraph.js
+++ src/common/styles/Paragraph.js
@@ -1,7 +1,24 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Paragraph = styled.p`
     line-height: 1.7;
     letter-spacing: 2px;
     padding: 20px;
+    margin: 0;
+
+    ${({ hiddenCard }) => hiddenCard && css`
+        display: inline-block;
+        width: 250px;
+        height: 200px;
+        overflow: hidden;
+        overflow-y: visible;
+        margin: 20px 0;
+        cursor: pointer;
+        transition: all .5s ease-in-out;
+
+        &:hover {
+            scale: 1.1;
+        }
+        
+    `}
 `;
