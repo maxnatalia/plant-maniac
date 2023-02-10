000b8596e9778948dcbd06eb88283212e9e5ef99
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-10 11:58:44 +0100

    updated Introduction component

--- src/features/HomePage/Introduction/index.js
+++ src/features/HomePage/Introduction/index.js
@@ -1,8 +1,9 @@
-import { Header } from "../../../common/styles/Header"
-import { Paragraph } from "../../../common/styles/Paragraph"
-import { Section } from "../../../common/styles/Section"
+import { Header } from "../../../common/styles/Header";
+import { Paragraph } from "../../../common/styles/Paragraph";
+import { Section } from "../../../common/styles/Section";
+import { ButtonLink } from "../../../common/styles/ButtonLink";
 import { data } from "./data";
-import { Container, ImageWrapper, Image, Wrapper } from "./styled.js";
+import { Container, ImageWrapper, Image, Wrapper, ArticleWrapper, Article, IconSpan } from "./styled.js";
 import plant1 from "./plant-one.jpg";
 import plant2 from "./plant-two.jpg";
 import plant3 from "./plant-three.jpg";
@@ -14,6 +15,17 @@ const Introduction = () => {
             <Header>Indoor Plant Cultivation at Home</Header>
             <Container>
                 <Wrapper>
+                    <Paragraph>
+                        Having plants in the home brings many benefits for our health and well-being.
+                        They serve as natural air filters, helping to remove harmful substances such as formaldehyde, benzene, and trichloroethylene.
+                        At night, they release carbon dioxide, which helps to maintain good air quality and improve sleep quality.
+                    </Paragraph>
+                    <Paragraph>
+                        Studies show that the presence of plants in the workplace can improve concentration and productivity.
+                        Plants are associated with better well-being and fewer symptoms of depression.
+                        Additionally, plants naturally regulate temperature and humidity,
+                        which can help to save energy and lower heating and air conditioning bills.
+                    </Paragraph>
                     <Paragraph>
                         Cultivating indoor plants is a hobby that is gaining popularity among those who want to increase
                         their green experience and improve the air quality in their homes.
@@ -28,16 +40,16 @@ const Introduction = () => {
                     <Image photoThree src={plant3} alt="plant photo3" />
                 </ImageWrapper>
             </Container>
-
-            <div>
+            <ArticleWrapper>
                 {data.map(({ id, title, text, icon }) => (
-                    <article key={id}>
-                        <div>{icon}</div>
-                        <h3>{title}</h3>
-                        <p>{text}</p>
-                    </article>
+                    <Article key={id}>
+                        <IconSpan>{icon}</IconSpan>
+                        <Header small>{title}</Header>
+                        <Paragraph hiddenCard>{text}</Paragraph>
+                        <ButtonLink to="/products">products</ButtonLink>
+                    </Article>
                 ))}
-            </div>
+            </ArticleWrapper>
         </Section>
 
     )
