3d8927f0cbefc3ca0b34e12e7ea14508482a66f8
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-12 08:19:14 +0100

    updated Footer component

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,13 +1,32 @@
-import { StyledFooter } from "./styled";
-import { Button } from "../../common/styles/Button"
+import { StyledFooter, Wrapper, Container, IconFace, IconInsta, IconMail, Item } from "./styled";
+import { ButtonLink } from "../../common/styles/ButtonLink"
 import { Header } from "../../common/styles/Header";
+import Logo from "../Logo";
+
 
 const Footer = () => {
     return (
         <StyledFooter>
-            <Header small>plant maniac &copy; {new Date().getFullYear()} </Header>
-            <Header small>all rights reserved</Header>
-            <Button to="/about">suscribe</Button>
+            <Wrapper>
+                <div>
+                    <Logo />
+                    <Container>
+                        <IconFace />
+                        <IconInsta />
+                        <IconMail />
+                    </Container>
+                </div>
+                <Container links>
+                    <Item>contact</Item>
+                    <Item>about</Item>
+                    <Item>blog & tips</Item>
+                </Container>
+            </Wrapper>
+            <Header small>plant maniac &copy; {new Date().getFullYear()} all rights reserved</Header>
+
+
+            {/* <ButtonLink to="/about">suscribe</ButtonLink> */}
+
         </StyledFooter>
     )
 }
