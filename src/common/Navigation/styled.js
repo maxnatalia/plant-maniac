893290afd6cb4083dedd72894439960c064b5db6
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-13 13:07:06 +0100

    improved Navigation styles

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -6,134 +6,176 @@ import { BsFillPersonFill } from "react-icons/bs";
 import styled from "styled-components";
 
 export const StyledNavigation = styled.nav`
-    width: 100%;
-    position: sticky;
-    padding: 5px 10px;
+    height: 90px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     gap: 10px;
+    position: relative;
 `;
 
 export const NavLinksWrapper = styled.ul`
-    flex:1;
+    flex: 1;
+    height: 100%;
     list-style: none;
     padding-left: 0;
+    margin: 0;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-around;
+    align-items: center;
 
-    @media (max-width: 767px) {
-        opacity: ${({ openNav }) => openNav ? "1" : "0"};
+    @media (max-width: 992px) {
+        opacity: ${({ openNav }) => openNav ? "1" : "0"};      
         visibility: ${({ openNav }) => openNav ? "visible" : "hidden"};
+        position: fixed;
         flex-direction: column;
-        align-items: center;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        z-index: 10;
+        height: 100vh;
+        width: 100%;
+        background-color: red;
+    }   
+`;
+
+export const NavLinkItem = styled.li`
+    cursor: pointer;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &::before {
+        content: "";
         position: absolute;
         top: 0;
         left: 0;
-        height: 100vh;
+        background-color: ${({ theme }) => theme.colors.primary};
+        width: 4px;
+        height: 100%;
+        transform: scaleY(0);
+        transition: transform .2s,
+                    width .4s cubic-bezier(1,0,0,1) .2s,
+                    background-color .1s;
+    }
+
+    &:hover::before {
+        transform: scaleY(1);
+        border-left: 4px solid ${({ theme }) => theme.colors.dark};
         width: 100%;
-        background-color: ${({ openNav }) => openNav ? "#e8f5e9" : "transparent"};
-        z-index: 2;
-        overflow: hidden;
     }
 `;
 
 export const StyledLink = styled(NavLink)`
-        color: ${({ theme }) => theme.colors.dark};
-        background-color: transparent;
-        text-decoration: none;
-        text-transform: uppercase;
-        padding: 10px 15px;
-        transition: all .2s;
-        cursor: pointer;
-        z-index: 2;
-    
-        &:hover {
-            box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
-        }
-
-        &.active {
-            box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
-        }
-`;
-export const BasketWrapper = styled.div`
+    color: inherit;
+    text-decoration: none;
+    text-transform: uppercase;
     display: flex;
-    flex-direction: column-reverse;
-    padding: 10px;
-    transition: 1s ease-in-out;
-    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    position: relative;
+    z-index: 10;
 
-    &:hover {
-        box-shadow: 1px 2px 10px ${({ theme }) => theme.colors.dark};
+    &.active {
+        font-weight: 900;
     }
 `;
 
+export const BasketWrapper = styled.div`
+    position: relative;
+    margin-right: 14px;
+`;
+
 export const BasketIcon = styled(SlBasket)`
-    font-size: 20px;
+    width: 30px;
+    height: 30px;
 `;
 
 export const TotalBasket = styled.span`
+    background-color: ${({ theme }) => theme.colors.dark};
+    color: ${({ theme }) => theme.colors.white};
+    font-weight: 100;
     border-radius: 50%;
+    position: absolute;
+    top: -12px;
+    right: -12px;
+    font-size: 14px;
     padding: 5px;
-    background-color: rgb(27, 94, 32, 0.5);
-    font-size: 12px;
-    font-weight: 600;
+    display: flex;
+    letter-spacing: 0;
+    justify-content: center;
+    align-items: center;
 `;
 
-export const LinkWrapper = styled(Link)`
+export const LoginWrapper = styled(Link)`
+    padding: 10px;
+    height: 100%;
     display: flex;
-    align-items:center;
-    transition: 1s ease-in-out;
-    cursor:pointer;
+    align-items: center;
+    color: inherit;
+    text-decoration: none;
+    text-transform: capitalize;
+    border-bottom: 4px solid transparent;
+    cursor: pointer;
+    transition: all .8s ease-in-out;
 
     &:hover {
-        box-shadow: 1px 2px 10px ${({ theme }) => theme.colors.dark};
+        border-bottom: 4px solid ${({ theme }) => theme.colors.dark};
+        background-color: ${({ theme }) => theme.colors.primary};
     }
 `;
 
 export const LoginIcon = styled(BsFillPersonFill)`
-    font-size: 30px;
-    color: ${({ theme }) => theme.colors.dark};
+    width: 30px;
+    height: 30px;
+    fill: currentColor;
 `;
 
 export const LoginText = styled.p`
-    font-size: 14px;
+    margin:0;
+    width: 80px;
+`;
 
-    @media (max-width: 767px) {
-        display: none;
+export const MobileClose = styled(AiOutlineClose)`
+    display: none;
+
+    @media (max-width: 992px) {
+        display: flex;
+        margin-right: 10px;
+        width: 40px;
+        height: 40px;
+        fill: currentColor;
+        cursor: pointer;
+        transition: all .5s ease-in-out;
+        position: relative;
+        z-index: 10;
+
+        &:hover {
+            transform: rotate(180deg) scale(1.3);
+        }
     }
 `;
 
 export const MobileOpen = styled(AiOutlineBars)`
     display: none;
 
-    @media (max-width: 767px) {
+    @media (max-width: 992px) {
         display: flex;
+        margin-right: 10px;
+        width: 40px;
+        height: 40px;
+        fill: currentColor;
         cursor: pointer;
-        font-size: 30px;
-        transition: 1s ease-in-out;
-        position: sticky;
-        z-index: 2;
+        transition: all .5s ease-in-out;
 
         &:hover {
-            transform: scale(1.2);
-        }
-    }
-`;
-
-export const MobileClose = styled(AiOutlineClose)`
-    display: none;
-
-    @media (max-width: 767px) {
-        display: flex;
-        cursor: pointer;
-        font-size: 30px;
-        transition: 1s ease-in-out;
-        position: sticky;
-        z-index: 2;
-
-        &:hover {
-            transform: scale(1.2);
+            transform: rotate(90deg) scale(1.3);
         }
     }
 `;
