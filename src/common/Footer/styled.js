416730073e3e96749cbefd672b9d631598a2c43d
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-15 10:47:07 +0100

    updated styles for Footer

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -1,60 +1,69 @@
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
-import { BsFacebook } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
-import { FiMail } from "react-icons/fi";
 
 export const StyledFooter = styled.footer`
-    
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
     padding: 20px;
     border-top: 2px solid ${({ theme }) => theme.colors.dark};
+    text-align: center;
 `;
 
 export const Wrapper = styled.div`
-    width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.dark};
 
-    @media (max-width: 767px) {
+    @media (max-width: 992px) {
         flex-direction: column;
     }
 `;
 
 export const Container = styled.div`
-    padding: 20px 30px;
+    padding: 20px;
     display: flex;
-    justify-content: space-around;
     align-items: center;
+    justify-content: center;
 
-    ${({ links }) => links && css`
+    ${({ items }) => items && css`
         flex: 1;
-        width: 100%;
+        flex-wrap: wrap;
+        order: 1;
+        gap: 30px;
     `}
 `;
 
-export const IconFace = styled(BsFacebook)`
+export const Icon = styled.div`
     font-size: 32px;
+    padding: 5px;
     cursor: pointer;
-`;
-export const IconInsta = styled(BsInstagram)`
-    font-size: 32px;
-    cursor: pointer;
-`;
+    transition: all .7s ease-in-out;
 
-export const IconMail = styled(FiMail)`
-    font-size: 32px;
-    cursor: pointer;
+    &:hover {
+        filter: brightness(130%);
+        transform: scale(1.2);
+    }
+
+    @media (max-width: 767px) {
+        font-size: 20px;
+    }
 `;
 
 export const Item = styled(Link)`
-    text-decoration: unset;
+    text-decoration: none;
+    color: inherit;
     text-transform: capitalize;
     letter-spacing: 2px;
     font-size: 20px;
+    font-weight: 600;
+    padding: 10px 20px;
+    background-color: ${({ theme }) => theme.colors.primary};
     cursor: pointer;
+    transition: all .3s ease; 
+
+    &:hover,
+    &:active {
+        font-weight: 900;
+        box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+        transform: rotate(5deg) scale(1.3);
+    }
 `;
