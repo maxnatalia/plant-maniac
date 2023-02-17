3972cedd4a7f5c70c9d35c1b4125a85abd689349
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-17 09:53:09 +0100

    added functionality to Newsletter msg and error

--- src/common/Newsletter/index.js
+++ src/common/Newsletter/index.js
@@ -1,10 +1,37 @@
+import { useState, useEffect } from "react";
 import { Header } from "../styles/Header";
 import { Paragraph } from "../styles/Paragraph";
 import { Section } from "../styles/Section";
 import leaves from "./leaves.mp4";
-import { Form, Input, SubmitButton, Container, VideoContainer, Wrapper } from "./styled";
+import { Form, Input, SubmitButton, Container, VideoContainer, Wrapper, MsgSuscribe } from "./styled";
 
 const Newsletter = () => {
+    const [email, setEmail] = useState("");
+    const [msg, setMsg] = useState(false);
+    const [error, setError] = useState(false);
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (email) {
+            setMsg(`Thank you ---${email}--- for subscribing to our newsletter`);
+            setEmail("");
+            setError(false);
+        }
+
+        if (!email) {
+            setError(true);
+            setMsg(`Please provide your e-mail`);
+        }
+    }
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setMsg(false);
+        }, 3000);
+        return () => clearTimeout(timeout);
+    }, [email, msg]);
+
     return (
         <Section>
             <Wrapper>
@@ -16,12 +43,22 @@ const Newsletter = () => {
                         <Header>newsletter</Header>
                         <Paragraph>Join our newsletter and get 20% off</Paragraph>
                     </div>
-                    <Form>
-                        <Input type="email" placeholder="Enter your e-mail..." />
+                    <Form onSubmit={onFormSubmit}>
+                        <Input error={error} type="email" placeholder="Enter your e-mail..." value={email} onChange={(e) => setEmail(e.target.value)} />
                         <SubmitButton type="submit">suscribe</SubmitButton>
                     </Form>
                 </Container>
             </Wrapper>
+            {error && msg &&
+                <MsgSuscribe>
+                    <Paragraph>{msg}</Paragraph>
+                </MsgSuscribe>
+            }
+            {msg &&
+                <MsgSuscribe>
+                    <Paragraph> {msg}</Paragraph>
+                </MsgSuscribe>
+            }
         </Section>
     )
 }
