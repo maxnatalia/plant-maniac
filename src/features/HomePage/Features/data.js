0a51238bef0870bb327b8e6439421c755ce885e7
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-18 09:16:44 +0100

    added data for Features component

---
+++ src/features/HomePage/Features/data.js
@@ -0,0 +1,32 @@
+export const features = [
+    {
+        id: 1,
+        title: 'Wide selection of indoor plants',
+        description: 'We offer a wide selection of indoor plants, so every customer will find something for themselves.'
+    },
+    {
+        id: 2,
+        title: 'High quality products',
+        description: 'We guarantee the highest quality of indoor plants, so our customers are satisfied with their purchase.'
+    },
+    {
+        id: 3,
+        title: 'On-time delivery',
+        description: 'We deliver ordered indoor plants on time, so our customers can enjoy them as soon as possible.'
+    },
+    {
+        id: 4,
+        title: 'Professional customer service',
+        description: 'Our team of employees is ready to help and provide advice on indoor plant care.'
+    },
+    {
+        id: 5,
+        title: 'Affordable prices',
+        description: 'We offer competitive prices for the high quality of our indoor plants.'
+    },
+    {
+        id: 6,
+        title: 'Easy and friendly ordering process',
+        description: 'Our online store with indoor plants offers an easy and friendly ordering process, so our customers have the best shopping experience.'
+    }
+];
