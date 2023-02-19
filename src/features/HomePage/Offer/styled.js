1e75c59a2c24905ef6d277ebffbc34bd506e0f36
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-19 10:08:29 +0100

    divided articles in Offer to other page Blog

--- src/features/HomePage/Offer/styled.js
+++ src/features/HomePage/Offer/styled.js
@@ -1,21 +1,10 @@
 import styled, { css } from "styled-components";
 import housePlant from "./house-plant.jpg";
 import officePlant from "./office-plant.jpg";
-import background from "./background.jpg";
 
 export const Article = styled.article`
     height: 100%;
     width: 100%;
-
-    ${({ space }) => space && css`
-        display: flex;
-        flex-direction: column;
-        background-blend-mode: screen;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: top;
-        background-image: linear-gradient(to right bottom, ${({ theme }) => theme.colors.white}, ${({ theme }) => theme.colors.primary}), url("${background}");
-    `}
 `;
 
 export const SpaceContainer = styled.div`
@@ -102,56 +91,3 @@ export const ContentWrapper = styled.div`
     `}
 `;
 
-export const BtnContainer = styled.div`
-    width: 100%;
-    margin: 40px auto;
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    gap: 20px;
-
-    @media (max-width: 767px) {
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 80px;
-        margin-bottom: 60px;
-    }
-`;
-
-export const Image = styled.img`
-    width: 250px;
-    height: 250px;
-    object-fit: cover;
-    border-radius: 10px;
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
-    transition: all .5s ease-in-out;
-    
-    ${({ group }) => group && css`
-        cursor: pointer;
-
-        &:hover {
-            filter: grayscale(80%);
-        }
-
-        @media (max-width: 767px) {
-            display: none;
-        }
-    `}
-`;
-
-export const Container = styled.div`
-    width: 250px;
-    position: relative;
-    transition: all .3s ease-in-out;
-    opacity: ${({ show }) => show ? 1 : 0};
-    animation: ${({ show }) => show ? "fadeIn 3s ease-in-out" : "fadeOut 3s ease-in-out"};
-    
-    &:hover > button {
-        transform: scale(1.3);  
-    }
-
-    ${({ disabled }) => disabled && css`
-        display: none;
-    `} 
-`;
