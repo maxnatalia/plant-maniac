48ab38fa30f1d347384b73a57dae76b1dccb9f56
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-19 10:10:18 +0100

    added data to Blog spaces and plantAccessories

---
+++ src/features/Blog/plantAccessories.js
@@ -0,0 +1,28 @@
+import pots from "./pots.jpg";
+import sprinklers from "./sprinklers.jpg";
+import reflectors from "./reflectors.jpg";
+
+
+export const plantAccessories = [
+    {
+        id: 1,
+        title: "Pots and Vases",
+        img: pots,
+        description:
+            "Pots and vases are an indispensable element in our garden and home. Pots should be chosen according to the needs of our plants. For example, plants with deep roots require deeper pots, while those with shallow roots can be grown in shallow pots. Vases are great if we want our plants to be more exposed. Flowered vase compositions are easy to maintain and add an elegant accent to our interior."
+    },
+    {
+        id: 2,
+        title: "Sprinklers",
+        img: sprinklers,
+        description:
+            "Plants need regular watering to grow and develop properly. Sprinklers are necessary if we want our plants to be well-watered without the need for daily watering. We can choose between manual sprinklers and automatic watering systems, depending on our needs and budget."
+    },
+    {
+        id: 3,
+        title: "Fluorescents and Reflectors",
+        img: reflectors,
+        description:
+            "Plants need light to produce chlorophyll and grow. If our plants do not receive enough light, they may become pale and weaker. Fluorescents and reflectors are an excellent solution if we want to ensure that our plants receive the right amount of light. These accessories are especially useful for plants that are placed in areas with low light."
+    }
+];
---
+++ src/features/Blog/spaces.js
@@ -0,0 +1,31 @@
+import bedroom from "./bedroom.jpg";
+import bathroom from "./bathroom.jpg";
+import kitchen from "./kitchen.jpg";
+import living from "./living-room.jpg";
+
+export const paragraphs = [
+    {
+        id: 1,
+        img: bedroom,
+        name: "bedroom",
+        text: "Plants such as lavender, jasmine, and Aloe Vera are good to grow in the bedroom. They have calming properties and can improve the quality of sleep.",
+    },
+    {
+        id: 2,
+        img: bathroom,
+        name: "bathroom",
+        text: "Plants such as Tradescantia, Pothos, Spathiphyllum, and Philodendron thrive in the warm and humid conditions of a bathroom. They are able to process large amounts of moisture and are good air filters.",
+    },
+    {
+        id: 3,
+        img: kitchen,
+        name: "kitchen",
+        text: "Plants such as tomatoes, green peppers, basil, and rosemary are good options for growing in the kitchen. They not only provide fresh herbs, but also help to clean the air of some toxins.",
+    },
+    {
+        id: 4,
+        img: living,
+        name: "livingroom",
+        text: "Plants such as the Ficus Benjamina, Dracaena, and Monstera are good options for growing in the living room. They are low maintenance and can tolerate average humidity and light conditions.",
+    }
+];
