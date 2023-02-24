c79d565d6993e9708e676f7cd71e7d65abfc4352
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-24 07:49:30 +0100

    improved loginContex file

--- src/context/loginContext.js
+++ src/context/loginContext.js
@@ -1,16 +1,15 @@
 import { useContext, useState, createContext } from 'react'
 
-const LoginContext = createContext()
-export const LoginProvider = ({ children }) => {
+const LoginContext = createContext();
 
+export const useUserContext = () => useContext(LoginContext);
+
+export const LoginProvider = ({ children }) => {
     const [message, setMessage] = useState("LOG IN 🪴");
+
     return (
         <LoginContext.Provider value={{ message, setMessage }}>
             {children}
         </LoginContext.Provider>
     )
-}
-
-export const useUserContext = () => {
-    return useContext(LoginContext);
-}
+};
